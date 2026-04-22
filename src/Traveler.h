7e4f792ec9b01b799373f4990abca276e47a50a5
Brook <30013859@cc.peralta.edu>
2026-04-22 08:18:00 +0000

    Initial project structure

--- src/Traveler.h
+++ src/Traveler.h
@@ -7,13 +7,13 @@
  * Traveler class represents a person planning a trip.
  * It stores their name, destination, and bugget. 
  */
-using namespace std;
+
 
 class Traveler {
 private:
     std::string name;
     std::string destination;
-    std::double budget;
+    double budget;
 
 public:
 
@@ -21,9 +21,9 @@ public:
 
     Traveler(const std::string&name, std::string destination, double budget);
 
-    std::string getName();
-    std::string getDestination();
-    std::double getBudget();
+    std::string getName() const;
+    std::string getDestination() const;
+    double getBudget();
 
     void displayInfo() const;
 };
